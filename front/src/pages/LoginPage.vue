14ee38b0f7dbb3d90b4b5e59bae9cbc2451f16a8
Luke859 <71589052+Luke859@users.noreply.github.com>
2023-01-09 09:11:21 +0100

    first commit

---
+++ front/src/pages/LoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <q-page class="flex flex-center">
+      <h1>Hello</h1>
+      <!-- <p>{{firstname}}</p> -->
+      <q-input type="test" v-model="firstname"/>
+      <q-btn @click="reverse" label="reverse"/>
+    </q-page>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const firstname = ref('luke')
+const reverse = () => {
+  firstname.value = firstname.value.split('').reverse().join('')
+}
+
+</script>
