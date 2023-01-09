d77a68df0899e2830ff94132897db0d7e0037c0a
Luke859 <71589052+Luke859@users.noreply.github.com>
2023-01-09 11:29:57 +0100

    changement api + style

--- front/src/router/routes.js
+++ front/src/router/routes.js
@@ -1,11 +1,13 @@
-
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: '/logIn', component: () => import('pages/LoginPage.vue') }
+      { path: '', name: 'homepage', component: () => import('pages/IndexPage.vue') },
+      { path: 'logIn', name: 'logIn', component: () => import('pages/LoginPage.vue') },
+      { path: 'tuto', name: 'tuto', component: () => import('pages/TutoPage.vue') },
+      { path: 'tuto/:id', name: 'tuto-params', component: () => import('src/pages/TutoPage.vue') },
+      { path: 'auth', name: 'auth', component: () => import('src/components/Auth/AuthPage.vue') }
     ]
   },
 
--- front/src/stores/example-store.js
+++ front/src/stores/example-store.js
@@ -1,15 +1,18 @@
-import { defineStore } from 'pinia';
+import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', {
   state: () => ({
-    counter: 0,
+    counter: 0
   }),
   getters: {
-    doubleCount: (state) => state.counter * 2,
+    doubleCount: (state) => state.counter * 2
   },
   actions: {
-    increment() {
-      this.counter++;
+    increment () {
+      this.counter++
     },
-  },
-});
+    decrement () {
+      this.counter--
+    }
+  }
+})
